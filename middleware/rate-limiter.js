c775c3e491cf9378040dcb41b2b7ca7315dfd334
XVI-IX <oladoja14@gmail.com>
2023-05-01 18:19:28 +0100

    Update ./middleware

---
+++ middleware/rate-limiter.js
@@ -0,0 +1,61 @@
+const { StatusCodes } = require("http-status-codes");
+
+const Redis = require('ioredis');
+const redis = new Redis();
+
+const limiter = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    const exists = redis.exists(token);
+
+    if (exists === 1) {
+      const result = redis.get(token);
+      const data = JSON.parse(result);
+      const currentTime = new Date().getTime();
+      const timeDiff = (currentTime - data.timeStamp) / 60000;
+
+      if (timeDiff >= 1) {
+        // if time limit exceeded
+        const body = {
+          count: 1,
+          timeStamp: new Date().getTime()
+        };
+
+        await redis.set(token, JSON.stringify(body));
+        next();
+      }
+      if (timeDiff < 1) {
+        // if within time limit
+        if (data.count > 3) {
+          // if allowed access times exceeded
+          res.status(StatusCodes.SERVICE_UNAVAILABLE).json({
+            error: -1,
+            msg: "API access limit exceeded"
+          });
+        }
+      }
+
+      data.count++;
+      await redis.set(token, JSON.stringify(data));
+
+      next();
+    } else {
+      // Create new redis key:value pair
+
+      const body = {
+        count: 1,
+        timeStamp: new Date().getTime()
+      }
+
+      await redis.set(token, JSON.stringify(body));
+      next();
+    }
+  } catch (error) {
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      error: -1,
+      msg: "Redis could not connect"
+    })
+  }
+}
+
+module.exports = limiter;
