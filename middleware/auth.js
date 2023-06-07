32a8cfdb416ee9e0b8f2edd2183b22db2f83a314
XVI-IX <oladoja14@gmail.com>
2023-06-07 05:07:48 +0100

    Fix: auth.js 2

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,32 +5,34 @@ const { Customer } = require("../models");
 const jwt = require('jsonwebtoken');
 
 const auth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError("Invalid credentials");
-  }
-
-  const token = authHeader.split(" ")[1];
 
   try {
-    const payload = jwt.verify(token, process.env.SECRET);
+    const authHeader = req.headers.authorization;
 
-    const customer = await Customer.findById(payload.userId).select(
-      '-password -address -age -phoneNumber -bvn -postalCode'
-    );
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      throw new UnauthenticatedError("Invalid credentials");
+    }
 
-    req.customer = customer;
+    const token = authHeader.split(" ")[1];
 
-    next();
+    try {
+      const payload = jwt.verify(token, process.env.SECRET);
+
+      const customer = await Customer.findById(payload.userId).select(
+        '-password -address -age -phoneNumber -bvn -postalCode'
+      );
+
+      req.customer = customer;
+
+      next();
+    } catch (error) {
+      console.log(error.message);
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
   } catch (error) {
-    console.log(error.message);
-    throw new UnauthenticatedError("Invalid Credentials");
+    console.log(error);
+    throw new UnauthenticatedError("Please Log in.")
   }
 }
-
-auth.catch(error => {
-  throw new BadRequestError(error);
-})
 
 module.exports = auth;
