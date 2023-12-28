472a320a8f968a1abafbe01ef40dbd8021d1f196
XVI-IX <oladoja14@gmail.com>
2023-12-28 09:07:16 +0100

    Add: add notification schema

---
+++ models/notifications.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  /**
+   * main content of the notification message
+   */
+  message: {
+    type: String,
+    required: true,
+  },
+  /**
+   * type or category of the notification
+   */
+  category: {
+    type: String,
+    required: true,
+    enum: ["Transaction", "Security Alert", "Account Update"],
+    default: "Account Update"
+  },
+  /**
+   * Indication of whether user has read the notification
+   */
+  status: {
+    type: String,
+    required: true,
+    default: "Unread",
+    enum: ["Read", "Unread"]
+  },
+  /**
+   * Priority level of notification
+   */
+  priority: {
+    type: String,
+    required: true,
+    default: "low",
+    enum: ["high", "medium", "low"]
+  },
+  /**
+   * Entity triggering the notification
+   */
+  sender_info: {
+    type: String,
+    required: true,
+    default: "System",
+    enum: ["System", "Transaction processing"]
+  },
+  expiration_date: {
+    type: Date,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Notification', notificationSchema);
