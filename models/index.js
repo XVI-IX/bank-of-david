ecb0b9780a03b2ce017bfcdfee9cf9ec0263b6ed
XVI-IX <oladoja14@gmail.com>
2023-12-28 10:38:15 +0100

    Update: models

--- models/index.js
+++ models/index.js
@@ -2,12 +2,20 @@ const User = require("./user");
 const Card = require("./card");
 const Transaction = require("./transactions");
 const Account = require("./accounts");
-const Schedule = require("./schedule")
+const Schedule = require("./schedule");
+const Beneficiary = require("./beneficiary");
+const Feedback = require("./feedback");
+const KYC = require("./kyc");
+const Notification = require("./notifications");
 
 module.exports = {
   User,
   Card,
   Transaction,
   Account,
-  Schedule
+  Schedule,
+  Beneficiary,
+  Feedback,
+  KYC,
+  Notification
 }
